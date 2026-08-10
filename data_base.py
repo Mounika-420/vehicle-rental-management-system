23e56eac28025683f801564b4ce451ea2aa3e275
Mounika-420 <mounikaselvarajp@gmail.com>
2026-08-10 22:19:48 +0530

    Fix Aiven MySQL connection

--- data_base.py
+++ data_base.py
@@ -1,18 +1,21 @@
-import pymysql
 import os
+import pymysql
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-class data_base:
+class Database:
     def __init__(self):
         self.conn = pymysql.connect(
             host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT", 3306)),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
-            charset="utf8"
+            charset="utf8",
+            ssl={"ssl": {}},
+            connect_timeout=30
         )
 
     def register(self, qry):
